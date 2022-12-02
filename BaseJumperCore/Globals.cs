259e76760d1560d961397c5f9056d354d3a8b778
uGuardian <Sean.Heavysege@gmail.com>
2022-12-01 18:58:32 -0700

    changed feature string

--- BaseJumperCore/Globals.cs
+++ BaseJumperCore/Globals.cs
@@ -1,6 +1,6 @@
 namespace BaseJumperAPI {
 	public static class Globals {
 		public const string Version = "0.0.6.3";
-		public const string feature = "develop";
+		public const string feature = "AbnoCards";
 	}
 }
